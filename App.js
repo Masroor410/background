5b16f7120615a19e47f7d534c2d1deeb33950dab
Masroor410 <130144394+Masroor410@users.noreply.github.com>
2024-11-05 14:38:16 +0500

    background remover using API key

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import RemoveBackground from './components/RemoveBackground';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <RemoveBackground/>
+    </div>
+  )
+}
